0930be99265f25e672af6165e462d4dd7eb206ac
Aditya Rajagopal <adi.raj465@gmail.com>
2023-10-22 10:52:24 -0700

    remove test benchmarking

--- Fracture/src/Fracture/Application.cpp
+++ Fracture/src/Fracture/Application.cpp
@@ -5,51 +5,6 @@
 
 namespace Fracture {
 
-
-	#define PROFILE_TESTING 0
-
-	#if PROFILE_TESTING ==	1
-	namespace benchmarks {
-
-		static void Trace()
-		{
-			FR_PROFILE_FUNCTION();
-
-			for (int i = 0; i < 100; i++)
-				FR_CORE_TRACE("Function1 {0}", i);
-		}
-
-		static void stdcout()
-		{
-			FR_PROFILE_FUNCTION();
-
-			for (int i = 0; i < 100; i++)
-				std::cout << "Function2 " << i << std::endl;
-		}
-
-		static void stdcoutnoendl()
-		{
-			FR_PROFILE_FUNCTION();
-
-			for (int i = 0; i < 100; i++)
-				std::cout << "Function2 " << i << "\n";
-		}
-
-		static void RunBenchmarks()
-		{
-			FR_PROFILE_FUNCTION();
-			FR_CORE_TRACE("Running Benchmarks");
-
-			std::thread a([]() { Trace(); });
-			std::thread b([]() { stdcout(); });
-			stdcoutnoendl();
-
-			a.join();
-			b.join();
-		}
-	}
-	#endif
-
 	Application::Application()
 	{
 	}
@@ -60,12 +15,6 @@ namespace Fracture {
 
 	void Application::Run()
 	{
-
-		FR_BEGIN_PROFILE_SESSION("Startup", "../Logs/FractureProfile-Startup.json");
-		#if PROFILE_TESTING == 1
-		benchmarks::RunBenchmarks();
-		#endif
-		FR_END_PROFILE_SESSION();
 		while (true);
 	}
 
